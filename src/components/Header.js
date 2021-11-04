1e7a000159bebaa9875272fb45e0155f7f62c185
Jrana1 <jewelbutex12@gmail.com>
2021-11-04 21:50:16 +0100

    animation added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import Flip from 'react-reveal/Flip';
 const Header = () => {
     return (
         <div style={{ textAlign: "center", marginBottom: "60px" }}>
-            <br /><br /><br /> <h2><span style={{ color: "#a75265", fontSize: "40px" }}>JRana's</span> Expense Tracker.</h2>
+            <br /><br /><br />
+            <Flip>
+                <h2><span style={{ color: "#a75265", fontSize: "40px" }}>JRana's</span> Expense Tracker.</h2>
+            </Flip>
         </div>
     );
 };
--- src/components/SingleHistory.js
+++ src/components/SingleHistory.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import Zoom from 'react-reveal/Zoom';
+
 
 const SingleHistory = ({ item, history, setAllInfo }) => {
     let d = new Date();
@@ -8,16 +10,17 @@ const SingleHistory = ({ item, history, setAllInfo }) => {
         setAllInfo(history.filter(itm => itm.id !== item.id));
     }
     return (
+        <Zoom top>
+            <div className={item.sign === "+" ? "itemsPlus" : "itemsMinus"}>
+                <p>{item.text}</p>
+                <p>{d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear()}</p>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                    <p style={{ marginRight: "15px" }}>{item.sign}{item.num}</p>
+                    <FontAwesomeIcon onClick={deleteHandler} className="icon-trash" icon={faTrash} />
+                </div>
 
-        <div className={item.sign === "+" ? "itemsPlus" : "itemsMinus"}>
-            <p>{item.text}</p>
-            <p>{d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear()}</p>
-            <div style={{ display: "flex", alignItems: "center" }}>
-                <p style={{ marginRight: "15px" }}>{item.sign}{item.num}</p>
-                <FontAwesomeIcon onClick={deleteHandler} className="icon-trash" icon={faTrash} />
             </div>
-
-        </div>
+        </Zoom>
 
     )
 }
